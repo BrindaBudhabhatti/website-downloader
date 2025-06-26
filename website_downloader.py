956e645a4da456bc435b67ed4b4f07cd8ce615e7
BrindaBudhabhatti <brinda.budhabhatti@gmail.com>
2025-06-26 18:33:50 +0530

    Downloades Website

--- website_downloader.py
+++ website_downloader.py
@@ -5,6 +5,8 @@ import os
 
 url = "https://cyber-wise-pi.vercel.app/"
 
+
+# Send a get request
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, "html.parser")
@@ -12,14 +14,45 @@ soup = BeautifulSoup(r.content, "html.parser")
 # Get list of links
 links = []
 for i in soup.find_all("a", href=True):
-    # Check if Links are not repeated
-    if i['href'] not in links:
+    # Check if links are not repeated
+    if (i['href'] not in links):
         links.append(i['href'])
 
-print("Links found: ")
+print("links found: ")
 print(links)
+
+# # Download Pages
+
+# # Make Dir
+# os.mkdir(f'Downloade_{url}_by_b')
+
+# for i in links: 
+
+#     # with open(f"Downloade_{url}_by_b/")
+
+# ------------------------------------------------------- 
 
 # Download Pages
 
-# Make Dir
-os.mkdir(f'Downloade_{url}_by_b')
+# Make Directory to save the project
+dir_name = f"Downloaded {urlparse("https://cyber-wise-pi.vercel.app/").netloc}"
+if not (os.path.isdir(dir_name)):
+    os.mkdir(dir_name)
+
+temp=1
+for i in links:
+    link = f"{url[:-1]}{i}"
+
+    r = requests.get(link)
+
+    if r.status_code != 200:
+        print("Error sending GET Request to the {link}")
+        break
+
+    with open(f'{dir_name}/{temp}.html', "wb") as f:
+        f.write(r.content)
+        print('{link} successfully downloaded!')
+        temp+=1
+
+
+print('Website Downloaded!')
