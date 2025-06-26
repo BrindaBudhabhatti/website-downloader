995b0e3811725bc81f3d073b316348283437e996
BrindaBudhabhatti <brinda.budhabhatti@gmail.com>
2025-06-26 18:40:35 +0530

    improved code

--- website_downloader.py
+++ website_downloader.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 import os
 
 url = "https://cyber-wise-pi.vercel.app/"
@@ -13,9 +13,10 @@ soup = BeautifulSoup(r.content, "html.parser")
 # Get list of links
 links = []
 for i in soup.find_all("a", href=True):
+    link = urljoin(url, i['href'])
     # Check if links are not repeated
-    if (i['href'] not in links):
-        links.append(i['href'])
+    if (link not in links):
+        links.append(link)
 
 print("links found: ")
 print(links)
@@ -23,23 +24,21 @@ print(links)
 # Download Pages
 
 # Make Directory to save the project
-dir_name = f"Downloaded {urlparse("https://cyber-wise-pi.vercel.app/").netloc}"
+dir_name = f"Downloaded {urlparse(url).netloc}"
 if not (os.path.isdir(dir_name)):
     os.mkdir(dir_name)
 
 temp=1
-for i in links:
-    link = f"{url[:-1]}{i}"
-
+for link in links:
     r = requests.get(link)
 
     if r.status_code != 200:
-        print("Error sending GET Request to the {link}")
+        print(f"Error sending GET Request to the {link}")
         break
 
     with open(f'{dir_name}/{temp}.html', "wb") as f:
         f.write(r.content)
-        print('{link} successfully downloaded!')
+        print(f'{link} successfully downloaded!')
         temp+=1
 
 
